9ebdaafd271e623fbb8ead680b02e4d835983670
Kromian1 <mok1408@yandex.ru>
2026-05-14 22:08:30 +0300

    добавлен sentry

--- config/logging.php
+++ config/logging.php
@@ -58,6 +58,16 @@ return [
             'ignore_exceptions' => false,
         ],
 
+        'channels' => [
+            // ...
+            'sentry_logs' => [
+                'driver' => 'sentry_logs',
+                // The minimum logging level at which this handler will be triggered
+                // Available levels: debug, info, notice, warning, error, critical, alert, emergency
+                'level' => env('LOG_LEVEL', 'info'), // defaults to `debug` if not set
+            ],
+        ],
+
         'single' => [
             'driver' => 'single',
             'path' => storage_path('logs/laravel.log'),
