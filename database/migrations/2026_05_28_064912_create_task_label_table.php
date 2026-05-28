2c7b96ea01d8747eac430af851990e2c5da1c759
m.kuznetsov <mok1408@yandex.ru>
2026-05-28 09:59:42 +0300

    создана промежуточная миграция task_label_table

---
+++ database/migrations/2026_05_28_064912_create_task_label_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('task_label', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('task_id')->unsigned();
+            $table->bigInteger('label_id')->unsigned();
+            $table->timestamps();
+
+            $table->foreign('task_id')->references('id')->on('tasks')->onDelete('cascade');
+            $table->foreign('label_id')->references('id')->on('labels')->onDelete('cascade');
+            $table->unique(['task_id', 'label_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('task_label');
+    }
+};
